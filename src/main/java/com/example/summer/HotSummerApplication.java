6def9474ce0ef7330d2d565f290f628f296ef3a7
ZWForum <782552186@qq.com>
2022-06-22 22:10:02 +0800

    initial

---
+++ src/main/java/com/example/summer/HotSummerApplication.java
@@ -0,0 +1,13 @@
+package com.example.summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotSummerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotSummerApplication.class, args);
+    }
+
+}
