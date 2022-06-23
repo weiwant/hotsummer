438a2d4140c177459a59143b2b6b10550c1ff186
Lily902 <2863368577@qq.com>
2022-06-23 10:44:06 +0800

    创建新项目

---
+++ src/components/LJY/ManagerUpload.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>这是上传页面</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Teacherhome from '../components/HSH/Teacherhome.vue'
+import Managerhome from '../components/LJL/Managerhome.vue'
+import ManagerUpload from '../components/LJY/ManagerUpload.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Teacherhome',
+    component: Teacherhome
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+	path: '/managerhome',
+	name: 'Managerhome',
+	component: Managerhome
+  },
+  {
+	path: '/managerupload',
+	name: 'ManagerUpload',
+	component: ManagerUpload
   }
 ]
 
 const router = new VueRouter({
-  routes
+	mode: 'history',
+	routes
 })
 
 export default router
