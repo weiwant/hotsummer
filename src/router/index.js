38be77a50289f8cd4927e057e1285d077f5e8e6e
Lily902 <2863368577@qq.com>
2022-06-24 15:52:08 +0800

    最新版本

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Teacherhome from '../components/HSH/Teacherhome.vue'
+import Teacherhome from '../components/HSH/teacher/Teacherhome.vue'
+import SpecialWorkload from '../components/HSH/teacher/SpecialWorkload.vue'
+import TeacherWorkloadTable from '../components/HSH/teacher/TeacherWorkloadTable.vue'
+import PersonalInfo from '../components/HSH/teacher/PersonalInfo.vue'
 import Managerhome from '../components/LJY/Managerhome.vue'
 import CheckTable from '../components/LJY/CheckTable.vue'
 import Login from '../components/LJY/Login.vue'
@@ -9,32 +12,39 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: 'login',
+    path: '/',
     name: 'Login',
     component: Login
+    //redirect: '/teacherhome'
   },
   {
     path: '/teacherhome',
     name: 'Teacherhome',
-    component: Teacherhome
+    component: Teacherhome,
+    children: [
+      { path: '', redirect: "PersonalInfo" },
+      { path: 'PersonalInfo', component: PersonalInfo },
+      { path: "SpecialWorkload", component: SpecialWorkload },
+      { path: "TeacherWorkloadTable", component: TeacherWorkloadTable },
+    ]
   },
   {
-	path: '/managerhome',
-	name: 'Managerhome',
-	component: Managerhome,
-  children: [
-    {
-      path: '/checktable',
-      name: 'CheckTable',
-      component: CheckTable
-    }
-  ]
+    path: '/managerhome',
+    name: 'Managerhome',
+    component: Managerhome,
+    children: [
+      {
+        path: '/checktable',
+        name: 'CheckTable',
+        component: CheckTable
+      }
+    ]
   },
 ]
 
 const router = new VueRouter({
-	mode: 'history',
-	routes
+  mode: 'history',
+  routes
 })
 
 export default router
