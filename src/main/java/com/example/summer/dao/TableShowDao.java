a928dfd6181285fb3a5ffda0f7f577a3b95e5d48
wwq <1980409739@qq.com>
2022-06-24 15:01:50 +0800

    按年份和学期查询

---
+++ src/main/java/com/example/summer/dao/TableShowDao.java
@@ -0,0 +1,55 @@
+package com.example.summer.dao;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.summer.entity.TeachingWorkloadStatistics;
+import com.example.summer.entity.Work;
+import com.example.summer.mapper.TeachingWorkloadStatisticsMapper;
+import com.example.summer.mapper.WorkMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.temporal.TemporalQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wwq
+ * @Date 2022/6/23 14:38
+ */
+@Repository
+public class TableShowDao {
+    @Autowired
+    WorkMapper workMapper;
+    @Autowired
+    TeachingWorkloadStatisticsMapper teacherMapper;
+
+    /**
+     * @Author： wwq
+     * @Return：
+     * @Description：获取数据库表所有内容
+     */
+    public List<TeachingWorkloadStatistics> getAllTables(){
+        List<TeachingWorkloadStatistics> works=teacherMapper.selectList(null);
+        return works;
+    }
+
+    /**
+     * @Author： wwq
+     * @Return：
+     * @Description： 根据年份返回对应数据表。数据库表的year字段
+     */
+    public List<TeachingWorkloadStatistics> getYearTable(String year) {
+        QueryWrapper<TeachingWorkloadStatistics> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("academic_year",year);
+        List<TeachingWorkloadStatistics> works=teacherMapper.selectList(queryWrapper);
+        return works;
+    }
+
+    public List<TeachingWorkloadStatistics> getSemesterTable(String year, int semester) {
+        QueryWrapper<TeachingWorkloadStatistics> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("academic_year",year);
+        queryWrapper.eq("semester",semester);
+        List<TeachingWorkloadStatistics> works=teacherMapper.selectList(queryWrapper);
+        return  works;
+    }
+}
---
+++ src/main/java/com/example/summer/service/impl/TableShowServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.summer.service.impl;
+
+import com.example.summer.dao.TableShowDao;
+import com.example.summer.entity.TeachingWorkloadStatistics;
+import com.example.summer.entity.Work;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author wwq
+ * @Date 2022/6/23 14:38
+ */
+@Service
+public class TableShowServiceImpl {
+    @Autowired
+    TableShowDao tableShowDao;
+
+    /**
+     * @Author： wwq
+     * @Return：
+     * @Description：获取数据库表所有内容
+     */
+    public List<TeachingWorkloadStatistics> getAllTables(){
+        List<TeachingWorkloadStatistics> WorkList=tableShowDao.getAllTables();
+        return WorkList;
+    }
+
+    /**
+     * @Author： wwq
+     * @Return：
+     * @Description： 根据年份返回对应数据表。数据库表的year字段
+     */
+    public List<TeachingWorkloadStatistics> getYearTable(String year) {
+        List<TeachingWorkloadStatistics> WorkInYear=tableShowDao.getYearTable(year);
+        return WorkInYear;
+    }
+
+    public List<TeachingWorkloadStatistics> getSemesterTable(String year, int semester) {
+        List<TeachingWorkloadStatistics> workInSemester=tableShowDao.getSemesterTable(year,semester);
+        return workInSemester;
+    }
+}
