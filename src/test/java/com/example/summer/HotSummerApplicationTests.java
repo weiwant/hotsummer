6bd856ca10adf4a39d441a93372cd8166a25582b
ZWForum <782552186@qq.com>
2022-06-25 15:01:30 +0800

    优化

---
+++ src/test/java/com/example/summer/HotSummerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotSummerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
