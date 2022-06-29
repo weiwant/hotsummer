d49f6722436a42a60963098585c548b6901ab8b3
ZWForum <782552186@qq.com>
2022-06-29 15:04:08 +0800

    initial

--- src/main/java/com/example/sprint2/Sprint2Application.java
+++ src/main/java/com/example/sprint2/Sprint2Application.java
@@ -1,8 +1,10 @@
 package com.example.sprint2;
 
+import com.tangzc.mpe.actable.EnableAutoTable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoTable
 @SpringBootApplication
 public class Sprint2Application {
 
