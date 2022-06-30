5318d19ba007da09bb2dc4bbe48d2408c66cff21
wwq <1980409739@qq.com>
2022-06-30 16:56:56 +0800

    考务工作量 其他

---
+++ src/main/java/com/example/sprint2/mybatis/service/ExaminationWorkloadService.java
@@ -0,0 +1,16 @@
+package com.example.sprint2.mybatis.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.sprint2.mybatis.entity.ExaminationWorkload;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author lenovo
+ * @since 2022-06-29
+ */
+public interface ExaminationWorkloadService extends IService<ExaminationWorkload> {
+
+}
---
+++ src/main/java/com/example/sprint2/mybatis/service/impl/ExaminationWorkloadServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.sprint2.mybatis.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.sprint2.mybatis.entity.ExaminationWorkload;
+import com.example.sprint2.mybatis.mapper.ExaminationWorkloadMapper;
+import com.example.sprint2.mybatis.service.ExaminationWorkloadService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author lenovo
+ * @since 2022-06-29
+ */
+@Service
+public class ExaminationWorkloadServiceImpl extends ServiceImpl<ExaminationWorkloadMapper, ExaminationWorkload> implements ExaminationWorkloadService {
+
+}
