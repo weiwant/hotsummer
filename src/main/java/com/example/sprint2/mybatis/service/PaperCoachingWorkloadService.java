eef907ec76fb2141fe95e2b0cdfff37247d3d32d
小黄同学 <2590685887@qq.com>
2022-06-30 19:55:48 +0800

    论文MP

---
+++ src/main/java/com/example/sprint2/mybatis/service/PaperCoachingWorkloadService.java
@@ -0,0 +1,16 @@
+package com.example.sprint2.mybatis.service;
+
+import com.example.sprint2.mybatis.entity.PaperCoachingWorkload;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 25906
+ * @since 2022-06-30
+ */
+public interface PaperCoachingWorkloadService extends IService<PaperCoachingWorkload> {
+
+}
