3a33c2bdb76cdfdd4c3194e9aff80c4f67881870
小黄同学 <2590685887@qq.com>
2022-07-01 10:56:18 +0800

    优化

--- src/main/java/com/example/sprint2/service/PaperService.java
+++ src/main/java/com/example/sprint2/service/PaperService.java
@@ -1,9 +1,21 @@
 package com.example.sprint2.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.example.sprint2.models.vo.PaperCoachingVo;
+import com.example.sprint2.mybatis.entity.PaperCoachingWorkload;
+
+import java.util.List;
+
 /**
  * @author hy
  * @Date 2022/6/29
  * @Description
  */
 public interface PaperService {
+    public List<PaperCoachingWorkload> selectAll();
+    public IPage<PaperCoachingWorkload> selectAllPage(PaperCoachingVo paperCoachingVo);
+    public List<PaperCoachingWorkload> selectYear(PaperCoachingVo paperCoachingVo);
+    public IPage<PaperCoachingWorkload> selectYearPage(PaperCoachingVo paperCoachingVo);
+    public List<PaperCoachingWorkload> selectTeacher(PaperCoachingVo paperCoachingVo);
+    public List<PaperCoachingWorkload> selectCourse(PaperCoachingVo paperCoachingVo);
 }
--- src/main/java/com/example/sprint2/service/impl/PaperServiceImpl.java
+++ src/main/java/com/example/sprint2/service/impl/PaperServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.example.sprint2.dao.PaperCoachingDao;
 import com.example.sprint2.models.vo.PaperCoachingVo;
 import com.example.sprint2.mybatis.entity.PaperCoachingWorkload;
+import com.example.sprint2.service.PaperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ import java.util.List;
  * @Description
  */
 @Service
-public class PaperServiceImpl {
+public class PaperServiceImpl implements PaperService {
     @Autowired
     PaperCoachingDao paperCoachingDao;
 
@@ -23,6 +24,7 @@ public class PaperServiceImpl {
      * @author hy
      * @description 不带分页的查询所有
      */
+    @Override
     public List<PaperCoachingWorkload> selectAll() {
         List<PaperCoachingWorkload> list = paperCoachingDao.selectAll();
         return list;
@@ -32,6 +34,7 @@ public class PaperServiceImpl {
      * @author hy
      * @description 带分页的查询所有
      */
+    @Override
     public IPage<PaperCoachingWorkload> selectAllPage(PaperCoachingVo paperCoachingVo) {
         IPage<PaperCoachingWorkload> iPage = paperCoachingDao.selectAllPage(paperCoachingVo);
         return iPage;
@@ -41,6 +44,7 @@ public class PaperServiceImpl {
      * @author hy
      * @description 不带分页查询所有某自然年记录
      */
+    @Override
     public List<PaperCoachingWorkload> selectYear(PaperCoachingVo paperCoachingVo) {
         List<PaperCoachingWorkload> list = paperCoachingDao.selectYear(paperCoachingVo);
         return list;
@@ -50,6 +54,7 @@ public class PaperServiceImpl {
      * @author hy
      * @description 带分页的查询所有某自然年记录
      */
+    @Override
     public IPage<PaperCoachingWorkload> selectYearPage(PaperCoachingVo paperCoachingVo) {
         IPage<PaperCoachingWorkload> iPage = paperCoachingDao.selectYearPage(paperCoachingVo);
         return iPage;
@@ -59,6 +64,7 @@ public class PaperServiceImpl {
      * @author hy
      * @description 不分页，按教师名称和自然年查询记录
      */
+    @Override
     public List<PaperCoachingWorkload> selectTeacher(PaperCoachingVo paperCoachingVo) {
         List<PaperCoachingWorkload> list = paperCoachingDao.selectTeacher(paperCoachingVo);
         return list;
@@ -68,6 +74,7 @@ public class PaperServiceImpl {
      * @author hy
      * @description 不分页，按课程名称和自然年查询记录
      */
+    @Override
     public List<PaperCoachingWorkload> selectCourse(PaperCoachingVo paperCoachingVo) {
         List<PaperCoachingWorkload> list = paperCoachingDao.selectCourse(paperCoachingVo);
         return list;
