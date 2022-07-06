4a9b95eca080817464a17b071569122a9be46f0e
黄诗慧 <2592311453@qq.com>
2022-07-07 06:51:22 +0800

    啊

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: {
-      'users': {
-        target: "http://abcds.vaiwan.com:8080/users",
-        ws: true,
-        changeOrigin: true,
-        pathRewrite: {
-          '^users': ''
-        }
-      }
-    }
+    // proxy: {
+    //   'users': {
+    //     target: "http://abcds.vaiwan.com:8080/users",
+    //     ws: true,
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^users': ''
+    //     }
+    //   }
+    // }
   }
 })
