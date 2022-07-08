59068ba0b07f131a5554eaad0ee88ef389580ad6
Lily902 <2863368577@qq.com>
2022-07-08 21:53:08 +0800

    token

--- src/main.js
+++ src/main.js
@@ -9,12 +9,13 @@ import axios from 'axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios;
-Vue.prototype.$domainName = "http://abkkds.vaiwan.com"  //axios请求域名
+Vue.prototype.$domainName = "http://abcd.vaiwan.com"  //axios请求域名
 Vue.prototype.$currentYear = (new Date()).getFullYear()  //当前的年份
 //当前用户的身份一定要延迟到home界面的created处获取，main.js会在login.vue之前执行
 Vue.prototype.$getUser = () => {
   Vue.prototype.$currentUser = localStorage.getItem("userName"); //当前用户的姓名
   Vue.prototype.$currentIdentity = localStorage.getItem("userIdentify"); //当前用户身份
+  // Vue.prototype.$token = localStorage.getItem("token");
 }
 
 //下载excel文件方法
@@ -32,6 +33,30 @@ Vue.prototype.$exportExcelFile = (dataToBeExported, header, filename) => {
     XLSX.writeFile(wb, filename + ".xlsx");
   });
 }
+localStorage.setItem("token", "");
+axios.interceptors.request.use(function(config) {
+    config.headers['token'] = localStorage.getItem("token") || '';
+    return config;
+  }, error => {
+
+  })
+
+axios.interceptors.response.use(response =>{
+  console.log(response);
+  let msg = response.data.response.code;
+  if(msg == 401){
+    alert("身份验证失败，请重新登陆！");
+    router.push('/');
+  }else if(msg == 403){
+    alert("无访问权限！");
+    if(localStorage.getItem("userIdentify") == 0){
+      router.push('/teacherhome');
+    }else if(localStorage.getItem("userIdentify") == 1 || localStorage.getItem("userIdentify") == 2 || localStorage.getItem("userIdentify") == 3){
+      router.push('/managerhome');
+    }
+  }
+  return response;
+}, error =>{})
 // // 特殊工作量类型(BB12文件里没给)
 // const SpecialWorkloadType = {
 //   BB1: "课程建设BB1",
